1be12e38f07341eab708da38e89c82977e5bc8a6
GEEK-213 <faariskhan213@gmail.com>
2025-02-26 17:34:20 +0530

    Feature Additions & Integration:
    Added Shop module along with required files
    Added About Us section with necessary assets
    Implemented Leaderboard with required files
    Linked all components together for seamless navigation

---
+++ aboutus.js
@@ -0,0 +1,39 @@
+
+// let contact = document.getElementById("contact-us");
+// console.log(contact);
+
+// let lastscrollY = window.scrollY;  
+
+// window.addEventListener("scroll", () => {
+//     let currscrollY = window.scrollY;
+
+//     if(currscrollY > lastscrollY){
+//         contact.style.bottom = "0";
+//     }
+//     else{
+//         contact.style.bottom = "-100vh";
+//     }
+
+//     lastscrollY = currscrollY;
+// })
+
+
+// let navbar = document.getElementById("navbar");
+// console.log(navbar);
+
+// let lastscrollY1 = window.scrollY;  
+
+// window.addEventListener("scroll", () => {
+//     let currscrollY1 = window.scrollY;
+
+//     if(currscrollY1 < lastscrollY1){
+//         navbar.style.top = "75px";
+//         navbar.style.zIndex = "99";
+//     }
+//     else{
+//         navbar.style.top = "0";
+//         navbar.style.zIndex = "-1";
+//     }
+
+//     lastscrollY1= currscrollY1;
+// })
